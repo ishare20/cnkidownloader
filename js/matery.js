92cc97a813f95649eac4ca8e9b74e300c22abaca
ishare20 <745926289@qq.com>
2019-01-15 14:45:00 +0800

    Site updated: 2019-01-15 14:44:53

---
+++ js/matery.js
@@ -0,0 +1,117 @@
+$(function () {
+    /**
+     * 添加文章卡片hover效果.
+     */
+    let articleCardHover = function () {
+        let animateClass = 'animated pulse';
+        $('article .article').hover(function () {
+            $(this).addClass(animateClass);
+        }, function () {
+            $(this).removeClass(animateClass);
+        });
+    };
+    articleCardHover();
+
+    /*菜单切换*/
+    $('.button-collapse').sideNav();
+
+    /* 修复文章卡片 div 的宽度. */
+    let fixPostCardWidth = function (srcId, targetId) {
+        let srcDiv = $('#' + srcId);
+        if (srcDiv.length === 0) {
+            return;
+        }
+
+        let w = srcDiv.width();
+        if (w >= 450) {
+            w = w + 21;
+        } else if (w >= 350 && w < 450) {
+            w = w + 18;
+        } else if (w >= 300 && w < 350) {
+            w = w + 16;
+        } else {
+            w = w + 14;
+        }
+        $('#' + targetId).width(w);
+    };
+
+    /**
+     * 修复footer部分的位置，使得在内容比较少时，footer也会在底部.
+     */
+    let fixFooterPosition = function () {
+        $('.content').css('min-height', window.innerHeight - 165);
+    };
+
+    /**
+     * 修复样式.
+     */
+    let fixStyles = function () {
+        fixPostCardWidth('navContainer', 'articles');
+        fixPostCardWidth('artDetail', 'prenext-posts');
+        fixFooterPosition();
+    };
+    fixStyles();
+
+    /*调整屏幕宽度时重新设置文章列的宽度，修复小间距问题*/
+    $(window).resize(function () {
+        fixStyles();
+    });
+
+    /*初始化瀑布流布局*/
+    $('#articles').masonry({
+        itemSelector: '.article'
+    });
+
+    AOS.init({
+        easing: 'ease-in-out-sine',
+        duration: 700,
+        delay: 100
+    });
+
+    /*文章内容详情的一些初始化特性*/
+    let articleInit = function () {
+        $('#articleContent a').attr('target', '_blank');
+
+        $('#articleContent img').each(function () {
+            let imgPath = $(this).attr('src');
+            $(this).wrap('<div class="img-item" data-src="' + imgPath + '"></div>');
+        });
+        $('#articleContent, #myGallery').lightGallery({
+            selector: '.img-item'
+        });
+
+        // progress bar init
+        const progressElement = window.document.querySelector('.progress-bar');
+        if (progressElement) {
+            new ScrollProgress((x, y) => {
+                progressElement.style.width = y * 100 + '%';
+            });
+        }
+    };
+    articleInit();
+
+    $('#toggleSearch').click(function () {
+        $('#searchModal').openModal();
+    });
+
+    /*回到顶部*/
+    $('#backTop').click(function () {
+        $('body,html').animate({scrollTop: 0}, 400);
+        return false;
+    });
+
+    /*监听滚动条位置*/
+    let $nav = $('#headNav');
+    let $backTop = $('.top-scroll');
+    $(window).scroll(function () {
+        /* 回到顶部按钮根据滚动条的位置的显示和隐藏.*/
+        let scroll = $(window).scrollTop();
+        if (scroll < 100) {
+            $nav.addClass('nav-transparent');
+            $backTop.slideUp(300);
+        } else {
+            $nav.removeClass('nav-transparent');
+            $backTop.slideDown(300);
+        }
+    });
+});
